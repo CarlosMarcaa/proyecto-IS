c486ab97098d85f6c9cf2ff4ec2b6f0f65b015d7
GAUGUSTO1602 <97412793+GAUGUSTO1602@users.noreply.github.com>
2024-06-04 01:15:48 -0400

    base-searcher-code

---
+++ repuestazo/lib/API.dart
@@ -0,0 +1,32 @@
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:repuestazo/Usermodel.dart';
+
+class FetchUser{
+
+  var data = [];
+  List<UserList> results = [];
+    String fetchurl = "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/474/modelyear/2015?format=json";
+    Future<List<UserList>>  getUserList({String? query}) async{
+        var url = Uri.parse(fetchurl);
+        var response = await http.get(url);
+        try {
+          if(response.statusCode == 200){
+
+          data = json.decode(response.body);
+        results = data.map((e) => UserList.fromJson(e)).toList();
+        if (query != null){
+          results = results.where((element) => element.makeName!.toLowerCase().contains(query.toLowerCase())).toList();
+        }
+
+        } else{
+          print('Error en la API');
+        }
+        } on Exception catch (e) {
+          print('error: $e');
+        }
+        return results;
+    }
+
+}
