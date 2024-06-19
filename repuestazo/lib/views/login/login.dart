17a11f71e70962d960997e66595416821da499fa
migue3210 <69486469+migue3210@users.noreply.github.com>
2024-06-19 18:43:03 -0400

    Update login.dart

--- repuestazo/lib/views/login/login.dart
+++ repuestazo/lib/views/login/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:repuestazo/views/login/body.dart';
-import 'package:repuestazo/views/home_page/home_page.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -9,19 +8,6 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Body(),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-            );
-          },
-          child: const Text("Holi"),
-        ),
-      ),
     );
   }
 }
-
