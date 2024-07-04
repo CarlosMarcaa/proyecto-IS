7ba084c12f06dc040dc788314b2eba0b3343f3d1
migue3210 <69486469+migue3210@users.noreply.github.com>
2024-07-04 04:15:45 -0400

    :sparkles:  Adds shopping cart and all its functionalities

--- repuestazo/lib/views/product_view/product_view.dart
+++ repuestazo/lib/views/product_view/product_view.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:repuestazo/views/product_view/body.dart';
-import 'package:repuestazo/models/item.dart';
 
-class ProductView extends StatefulWidget{
+class ProductView extends StatefulWidget {
   final String brand;
   final String category;
   final String description;
@@ -12,20 +12,21 @@ class ProductView extends StatefulWidget{
   final int price;
   final String userId;
 
-  ProductView({Key? key,
-    required this.brand,
-    required this.category,
-    required this.description,
-    required this.model,
-    required this.name,
-    required this.price,
-    required this.userId
-  }) : super(key: key);
+  ProductView(
+      {Key? key,
+      required this.brand,
+      required this.category,
+      required this.description,
+      required this.model,
+      required this.name,
+      required this.price,
+      required this.userId})
+      : super(key: key);
 
   ProductViewState createState() => ProductViewState();
 }
 
-class ProductViewState extends State<ProductView>{
+class ProductViewState extends State<ProductView> {
   late String brand;
   late String category;
   late String description;
@@ -33,8 +34,13 @@ class ProductViewState extends State<ProductView>{
   late String name;
   late int price;
   late String userId;
+  int quantity = 1;
 
-  void initState(){
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
     brand = widget.brand;
     category = widget.category;
     description = widget.description;
@@ -44,29 +50,160 @@ class ProductViewState extends State<ProductView>{
     userId = widget.userId;
   }
 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            color: Colors.white,
-            onPressed: () => {
-              Navigator.pop(context)
-            },
+  Future<bool> isCustomer() async {
+    User? user = auth.currentUser;
+    if (user != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      return userDoc['userType'] == 'Client';
+    }
+    return false;
+  }
+
+  Future<void> addToCart() async {
+    User? user = auth.currentUser;
+    if (user != null) {
+      QuerySnapshot existingProduct = await FirebaseFirestore.instance
+          .collection('cart')
+          .where('userId', isEqualTo: user.uid)
+          .where('name', isEqualTo: name)
+          .get();
+
+      if (existingProduct.docs.isNotEmpty) {
+        // Producto ya existe en el carrito, sumar la cantidad
+        DocumentSnapshot productDoc = existingProduct.docs.first;
+        int newQuantity = productDoc['quantity'] + quantity;
+        await FirebaseFirestore.instance
+            .collection('cart')
+            .doc(productDoc.id)
+            .update({'quantity': newQuantity});
+      } else {
+        // Producto no existe en el carrito, añadir nuevo
+        await FirebaseFirestore.instance.collection('cart').add({
+          'userId': user.uid,
+          'brand': brand,
+          'category': category,
+          'description': description,
+          'model': model,
+          'name': name,
+          'price': price,
+          'quantity': quantity,
+        });
+      }
+    }
+  }
+
+  void _showConfirmationDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirmación'),
+          content:
+              Text('¿Estás seguro que deseas añadir este producto al carrito?'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Añadir'),
+              onPressed: () async {
+                await addToCart();
+                Navigator.of(context).pop();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Producto añadido al carrito')),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              ListTile(
+                title: Text('Name: $name'),
+              ),
+              ListTile(
+                title: Text('Brand: $brand'),
+              ),
+              ListTile(
+                title: Text('Description: $description'),
+              ),
+              ListTile(
+                title: Text('Model: $model'),
+              ),
+              ListTile(
+                title: Text('Price: $price'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.remove),
+                    onPressed: () {
+                      setState(() {
+                        if (quantity > 1) quantity--;
+                      });
+                    },
+                  ),
+                  Text('$quantity'),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      setState(() {
+                        quantity++;
+                      });
+                    },
+                  ),
+                ],
+              ),
+              FutureBuilder(
+                future: isCustomer(),
+                builder: (context, AsyncSnapshot<bool> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasData && snapshot.data!) {
+                    return Center(
+                      child: TextButton.icon(
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black,
+                        ),
+                        icon: Icon(Icons.add),
+                        label: Text('Añadir al carrito'),
+                        onPressed: _showConfirmationDialog,
+                      ),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            ],
           ),
         ),
-        body: Body(
-          brand: brand,
-          category: category,
-          description: description,
-          model: model,
-          name: name,
-          price: price,
-          userId: userId,
-        )
       ),
     );
   }
